7878b582f4123ec565f2b1eed73abb981ddf39b6
JonnierGrajalesAlzate <alzatejonny39@gmail.com>
2026-06-22 08:50:01 -0500

    feat: Realización de componente Topbar.jsx y carpeta Layout con DashboardLayout

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,16 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom"; 
+import { useNavigate } from "react-router-dom";
+import DashboardLayout from "../layouts/DashboardLayout"; 
 const Dashboard = () => {
   return (
     <div>
+      <DashboardLayout>
+                <div className="flex justify-center items-center h-[70vh]">
+                    <div className="bg-white p-8 rounded-3xl shadow-lg">
+                        Pendiente por hacer
+                    </div>
+                </div>
+            </DashboardLayout>
       <h1>¡Hola, Mundo!</h1>
     </div>
   );
