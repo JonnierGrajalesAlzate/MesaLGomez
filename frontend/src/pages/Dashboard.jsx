fd233558d42e2d7d16d60294d5eb4d4fe7eb0a97
JonnierGrajalesAlzate <alzatejonny39@gmail.com>
2026-06-22 08:29:24 -0500

    feat: Login realizado, tanto la parte del front como la verifivcación de los usuarios

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom"; 
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>¡Hola, Mundo!</h1>
+    </div>
+  );
+};
+export default Dashboard;
